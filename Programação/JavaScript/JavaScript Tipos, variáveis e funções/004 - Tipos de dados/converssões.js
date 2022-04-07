cd56d97aef49e4722690487bb37f34886b8d2c9f
Diechabral <th3d1eg0master@gmail.com>
2022-04-07 15:00:18 -0300

    adicionar aulas de converções

---
+++ Programação/JavaScript/JavaScript Tipos, variáveis e funções/004 - Tipos de dados/converssões.js
@@ -0,0 +1,12 @@
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString)
+//reconhecendo o numeor transformado em string e comparou os dois como string
+
+console.log(numero + numeroString)
+//opertador de soma na verdade é concatenação
+
+console.log(numero + Number(numeroString))
+//o operador funcional Number transforma a string pra numero
+
