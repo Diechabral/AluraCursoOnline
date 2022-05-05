ed631c2caf4366093124c34aeb9de3014d70c0b9
Diechabral <th3d1eg0master@gmail.com>
2022-05-05 10:22:18 -0300

    update

--- Programação/JavaScript/002 - Fundamentos do JavaScript Arrays/009 - Procurando Nó/no.js
+++ Programação/JavaScript/002 - Fundamentos do JavaScript Arrays/009 - Procurando Nó/no.js
@@ -1,8 +1,15 @@
-const alunos = ['Diego', 'marjorie', 'ricardo', 'alexandre', 'juliana']
+const alunos = ['João', 'Juliana', 'caio', 'Ana']
 const mediasDosAlunos = [10, 7, 9, 6]
 
 let listaDeNotasEAlunos = [alunos, mediasDosAlunos]
 
-const exibeNomeNota = (nomeAluno) => {
-
+const exibeNomeNota = (nomeDoAluno) => {
+    if (listaDeNotasEAlunos[0].includes(nomeDoAluno)) {
+        let indice = listaDeNotasEAlunos.indexOf(nomeDoAluno)
+        return listaDeNotasEAlunos[0][indice] + ',sua media é ' + listaDeNotasEAlunos[1][indice]
+    }
+    else {
+        return "Esse aluno não esta cadastrado"
+    }
 }
+console.log(exibeNomeNota("Ana"))
