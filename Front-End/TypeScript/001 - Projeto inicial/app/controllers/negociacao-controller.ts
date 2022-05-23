4ab9eff431fca861cb91bcd16219c1db7d487ae1
Diechabral <th3d1eg0master@gmail.com>
2022-05-23 08:06:45 -0300

    adding new classes to the log

--- Front-End/TypeScript/001 - Projeto inicial/app/controllers/negociacao-controller.ts
+++ Front-End/TypeScript/001 - Projeto inicial/app/controllers/negociacao-controller.ts
@@ -1,8 +1,26 @@
+import { Negociacao } from "../models/negociacao";
+
 export class NegociacaoController {
-    private inputData;
-    private inputQuantidade;
-    private inputValor;
+    private inputData: any;
+    private inputQuantidade: any;
+    private inputValor: any;
     constructor() {
-        this.inputData = document.querySelector ('#data');
+        this.inputData = document.querySelector('#data');
+        this.inputQuantidade = document.querySelector('#quantidade')
+        this.inputValor = document.querySelector('#valor')
     }
+
+    adiciona(): void {
+        const negociacao = this.criaNegociacao();
+        console.log(negociacao);
+    }
+
+    criaNegociacao(): Negociacao {
+        const exp = /-/g;
+        const date = new Date(this.inputData.value.replace(exp, ','));
+        const quantidade = parseInt(this.inputQuantidade.value);
+        const valor = parseFloat(this.inputValor.value);
+        return new Negociacao(date, quantidade, valor);
+    }
+
 }
