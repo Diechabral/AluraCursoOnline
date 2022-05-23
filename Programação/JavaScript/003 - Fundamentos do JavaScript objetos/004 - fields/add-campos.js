40e1d71833596b5bf569e3c9783aec135770993d
Diechabral <th3d1eg0master@gmail.com>
2022-05-23 08:09:39 -0300

    update de aulas

--- Programação/JavaScript/003 - Fundamentos do JavaScript objetos/004 - fields/add-campos.js
+++ Programação/JavaScript/003 - Fundamentos do JavaScript objetos/004 - fields/add-campos.js
@@ -5,8 +5,11 @@ const cliente = {
     email: "asdfsdf@gmail.com",
 }
 console.log(cliente)
+
 cliente.fone = "65498721"
+
 console.log(cliente)
+
 cliente.fone = "54613287"
-console.log(cliente
-)
+
+console.log(cliente)
